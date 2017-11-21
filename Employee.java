b92294fc788b000645028b38ed2e2e4cde83fc6f
CapDev <bryan.deleon@pointwest.com.ph>
2017-11-21 20:42:43 +0800

    gfgf

---
+++ Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+
+	private int employeeId;
+
+p	ublic Employee() {
+		this.employeeId = 1;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public int getEmployeeId() {
+	return employeeId;
+	}
+}
